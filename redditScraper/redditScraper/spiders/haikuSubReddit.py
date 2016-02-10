ddbb08bb2f352f7cfa37df150f4b43b2eed8aa72
Daniel Watt <dwatt@blackberry.com>
2016-02-09 21:24:27 -0500

    Updated spider
    The spider now includes word parsing and it creates a JSON file for the
    counting of the words

--- redditScraper/redditScraper/spiders/haikuSubReddit.py
+++ redditScraper/redditScraper/spiders/haikuSubReddit.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 import logging
+import operator
+import json
+import sys
+from unidecode import unidecode
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
 logger = logging.getLogger('mycustomlogger')
+wordDictionary = {}
+symbolList = {',','?','.','"','!',';','[',']','*',' -','- ', '(', ')'}
+replaceUnicodeList = { u'\u2019':'′' }
 
 class HaikuSubRedditSpider(CrawlSpider):
     name = "haikuSubReddit"
     allowed_domains = ["www.reddit.com"]
-    start_urls = ['http://www.reddit.com/r/haiku/']
-
+    start_urls = ['http://www.reddit.com/r/haiku/'] 
     rules = [
     	Rule(LinkExtractor(
     		allow=['/r/haiku/\?count=\d*&after=\w*']),
@@ -20,18 +26,26 @@ class HaikuSubRedditSpider(CrawlSpider):
     def parse_start_url(self, response):
         titles = response.xpath('//a[contains(@class, "title")]/text()').extract()
         for title in (titles):
-            logger.info(title)
-            title = title.replace(',','')
-            title = title.replace('?','')
-            title = title.replace('.','')
-            logger.info(title)
+            title = unidecode(title)
+            title = title.lower()
+            for symbol in symbolList:
+                title = title.replace(symbol,'')
+
             mylist = title.split("/")
             
             for lineNumber, line in enumerate(mylist): 
                 wordList = line.split(" ")
 
-                logger.info("Line Number %d" % lineNumber)
-                for wordNumber, word in enumerate(wordList): 
-                    logger.info("%d: %s" % (wordNumber,word))
+                for wordNumber, word in enumerate(wordList):
+                    word = word.replace(' ','') 
+                    if word != '':
+                        if word in wordDictionary:
+                            wordDictionary[word] += 1
+                        else:
+                            wordDictionary[word] = 1
+
+    def closed(self,reason):
+        jsonFile = open('wordCount.json', 'w')
+        print >>jsonFile, json.dumps(wordDictionary, sort_keys=True, indent=4, separators=(',',": "))
 
 
